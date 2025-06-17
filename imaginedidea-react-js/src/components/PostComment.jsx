850565c7eab35e536200f2e4b491c408a12b83b5
Paulade91 <paulannadebby@yahoo.com>
2025-06-17 12:57:32 +0000

    first commit of the component

---
+++ imaginedidea-react-js/src/components/PostComment.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+
+function PostComment({ post }) {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/1/comments`)
+      .then((res) => res.json())
+      .then((data) => setComments(data));
+  }, [post.id]);
+
+  return (
+    <div>
+      {comments.length === 0 ? (
+        <p>No comments yet.</p>
+      ) : (
+        <ul>
+          {comments.map((comment) => (
+            <li key={comment.id}>
+              <strong>{comment.name}</strong>: {comment.body}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default PostComment;
